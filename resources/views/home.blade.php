79b9eb67fcc395c3392a20a5bffe5be649b0d7b2
sviat-o-slav <sviatoslav.voitenko@gmail.com>
2022-06-05 17:03:59 +0300

    header complete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,23 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Organic</title>
+        <link rel="stylesheet" href="css/app.css">
+        <title>Organic Shop</title>
     </head>
     <body>
-        <h1>Hello, Organic!</h1>
+        <header class="container-content">
+            <nav class="nav-main">
+                <a class="nav-logo-item" href="#"><img src="images/logo.svg" alt="Logo image"></a>
+
+                <ul class="nav-main-list">
+                    <li class="nav-main-item nav-main-item-active"><a href="#">Home</a></li>
+                    <li class="nav-main-item"><a href="#">About us</a></li>
+                    <li class="nav-main-item"><a href="#">Shop</a></li>
+                    <li class="nav-main-item"><a href="#">Pages</a></li>
+                    <li class="nav-main-item"><a href="#">Contact us</a></li>
+                    <li class="nav-main-item cart-item"><a href=""><span class="cart-icon"></span>Cart<span class="cart-badge">2</span></a></li>
+                </ul>
+            </nav>
+        </header>
     </body>
 </html>
